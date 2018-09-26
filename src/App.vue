f5a60d2b773a5ada365cea203837e46f8fb0a52f
notFromHere <jakubwasiljew@gmail.com>
2018-09-26 16:51:51 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-  </div>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
-  }
-}
+    let App = {
+        name: 'app',
+        components: {}
+    };
+    export default App;
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import * as firebase from 'firebase';
 import App from './App.vue'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App),
+    created() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAGdKF6HaQ3j3ti_XJZ4-KsjBOt8x_p8dM',
+            authDomain: 'vue-firebase-auth-22681.firebaseapp.com',
+            databaseURL: 'https://vue-firebase-auth-22681.firebaseio.com',
+            projectId: 'vue-firebase-auth-22681',
+            storageBucket: 'vue-firebase-auth-22681.appspot.com',
+            messagingSenderId: '48746280970'
+        })
+    }
 }).$mount('#app');
